52ec71f6bea3c1cc905b824024e5fe24a77227fb
Harvindsokhal <harvindsokhal@gmail.com>
2021-04-04 16:18:50 +0100

    added creds file

---
+++ creds.py
@@ -0,0 +1,8 @@
+InstagramUsername = 'ENTER USERNAME HERE'
+InstagramPassword = 'ENTER PASSWORD HERE'
+
+PinterestUsername = 'ENTER USERNAME HERE'
+PineterestPassword = 'ENTER PASSWORD HERE'
+
+FlickrUsername = 'ENTER USERNAME HERE'
+FlickrPassword = 'ENTER PASSWORD HERE'
